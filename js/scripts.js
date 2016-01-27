2f4bbdc1d6c5eb83c35290281726ba6cc477681b
brad pizzimenti and nic netzel <netzeln@gmail.com>
2016-01-26 16:02:59 -0800

    array loop discount logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,64 @@
-function Ticket(name,time,firstRun,senior,matinee) {
+function Ticket (name,time) {
   this.name = name;
   this.time = time;
-  this.firstRun = firstRun
-  this.senior = senior
-  this.matinee = matinee
+  this.senior = false;
+  this.matinee = false;
+  this.notFirstRun = false;
+  this.discounts = [];
+
+}
+
+Ticket.prototype.seniorDiscount = function() {
+  this.senior = true;
+  this.discounts.push(this.senior);
+}
+
+Ticket.prototype.matineeDiscount = function() {
+  this.matinee = true;
+  this.discounts.push(this.matinee);
+}
+
+Ticket.prototype.notFirstRunDiscount = function() {
+  this.notFirstRun = true;
+  this.discounts.push(this.notFirstRun);
 }
 
 Ticket.prototype.ticketPrice = function() {
-  var basePrice = 10;
-  if (this.firstRun === true && this.matinee === false && this.senior === false) {
-    return basePrice;
-  }
+// var numOfDiscounts = 0
+//   for (i = 0; i < this.discounts.length; i++) {
+//       if (this.discounts[i] === "true")
+//       numOfDiscounts++;
+//   }
+//   this.price -= numOfDiscounts
+var price = 10
+return price -= this.discounts.length;
 }
+
+var testTick = new Ticket();
+
+// function Ticket (name,time,discounts) {
+//   this.name = name;
+//   this.time = time;
+//   var notFirstRun = false;
+//   var matinee = false;
+//   var senior = false;
+//   this.discounts = [notFirstRun,matinee,senior]
+// }
+
+
+
+// function Ticket(name,time,notFirstRun,senior,matinee) {
+//   this.name = name;
+//   this.time = time;
+//   this.notFirstRun = notFirstRun
+//   this.senior = senior
+//   this.matinee = matinee
+// }
+
+// Ticket.prototype.ticketPrice = function() {
+//   var basePrice = 10;
+//
+//   if (this.notFirstRun === false && this.matinee === false && this.senior === false) {
+//     return basePrice;
+//   }
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,20 +1,20 @@
 describe("Ticket", function() {
   it("displays the name of a given movie", function() {
-    var testTicket = new Ticket ("Star Wars","17:30",true,false,false);
+    var testTicket = new Ticket ("Star Wars","17:30",false,false,false);
     expect(testTicket.name).to.equal("Star Wars");
     expect(testTicket.time).to.equal("17:30");
-    expect(testTicket.firstRun).to.equal(true);
+    expect(testTicket.notFirstRun).to.equal(false);
     expect(testTicket.senior).to.equal(false);
     expect(testTicket.matinee).to.equal(false);
   });
 
   it("display base price of a first run movie", function() {
-    var testTicket = new Ticket ("Star Wars","17:30",true,false,false);
-    expect(testTicket.name).to.equal("Star Wars");
-    expect(testTicket.time).to.equal("17:30");
-    expect(testTicket.firstRun).to.equal(true);
-    expect(testTicket.senior).to.equal(false);
-    expect(testTicket.matinee).to.equal(false);
+    var testTicket = new Ticket ("Star Wars","17:30",false,false,false);
+    expect(testTicket.ticketPrice()).to.equal(10);
+  });
+
+  it("display a first run attended by a senior", function() {
+    var testTicket = new Ticket ("Star Wars","17:30",false,true,false);
     expect(testTicket.ticketPrice()).to.equal(10);
   });
 });
